6459721947f0526b8928d77693f47c8f6dcb4e2b
Lior Carmeli <liorscarmeli@gmail.com>
2024-11-09 01:09:47 -0500

    Creating render pipeline (WIP)

--- src/renderer.rs
+++ src/renderer.rs
@@ -34,5 +34,68 @@ impl<'a> Renderer<'a> {
         .unwrap();
 
         let surface_caps = surface.get_capabilities(&adapter);
+
+        let camera_bind_group = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("Camera Bind Group"),
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+        });
+
+        let planet_bind_group = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("Planet Data Bind Group"),
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+        });
+
+        let bind_group_layouts = vec![&camera_bind_group, &planet_bind_group];
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &bind_group_layouts,
+                push_constant_ranges: &[],
+            });
+
+        let vertex_shader_module = device.create_shader_module(wgpu::include_wgsl!("../shaders/vertex.wgsl"));
+        let vertex_attributes = wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x2];
+
+        let vertex_shader = wgpu::VertexState {
+            module: &vertex_shader_module,
+            entry_point: "main",
+            compilation_options: Default::default(),
+            buffers: &[wgpu::VertexBufferLayout {
+                array_stride: std::mem::size_of::<[f32;5]>() as wgpu::BufferAddress,
+                step_mode: wgpu::VertexStepMode::Vertex,
+                attributes: &vertex_attributes,
+            }],
+        };
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: todo!(),
+            primitive: todo!(),
+            depth_stencil: todo!(),
+            multisample: todo!(),
+            fragment: todo!(),
+            multiview: todo!(),
+            cache: todo!(),
+        });
     }
 }
