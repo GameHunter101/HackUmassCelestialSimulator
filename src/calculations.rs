728b434ff773990d855c35e5a11748f02283b3ed
Key <46889185+Chakr3y@users.noreply.github.com>
2024-11-09 00:16:31 -0500

    arrgh

--- src/calculations.rs
+++ src/calculations.rs
@@ -1,4 +1,4 @@
-use nalgebra::Vector2;
+use nalgebra::Vector3;
 
 const GRAV: f32 = 6.67430;
 
@@ -7,6 +7,28 @@ struct Planet {
     radius: f32, // Display size, no calculation
     pos: Vector3<f32>,
     vel: Vector3<f32>,
-    accel: Vector3<f32>, // Do we need this?
 }
 
+impl Planet {
+    // Calculate acceleration
+    fn calcAccel(&mut self, planetList: &[Planet]) -> Vector3<f32> {
+        // Make sure planetList doesn't contain self
+        let mut accel: Vector3<f32> = 0;
+        for p in planetList {
+            let dr = p.pos - self.pos;
+            accel += p.mass * dr / dr.magnitude().powi(3);
+        }
+        accel * GRAV
+    }
+
+    // Do the actual moving
+    fn step(&mut self, planetList: &[Planet], dt: f32,) {
+        let pos_old = self.pos;
+        let vel_old = self.vel;
+        let accel_old = calcAccel(planetList);
+        self.pos = self.pos + self.vel * dt;                    // Expected pos
+        let accel_exp = calcAccel(planetList);
+        self.vel += (accel_old + accel_exp) * 0.5 * dt;         // New vel
+        self.pos = pos_old + (self.vel + vel_old) * 0.5 * dt;   // New pos
+    }
+}
