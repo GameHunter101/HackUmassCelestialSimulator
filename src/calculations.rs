8be2b905f0b9e473db6265ecf86434029ad826c3
Key <46889185+Chakr3y@users.noreply.github.com>
2024-11-09 13:26:00 -0500

    started calc init velocity function

--- src/calculations.rs
+++ src/calculations.rs
@@ -1,20 +1,27 @@
+use std::intrinsics::sqrtf32;
+
 use nalgebra::Vector3;
 
 const GRAV: f32 = 6.67430;
 
 struct Planet {
+    // Physical properties
     mass: f32,
-    radius: f32, // Display size, no calculation
     pos: Vector3<f32>,
     vel: Vector3<f32>,
+
+    // Display properties
+    id: Option<u8>,
+    radius: f32,
+    rot_vel: f32, // Angular velocity in rad/s
 }
 
 impl Planet {
     // Calculate acceleration
-    fn calcAccel(&self, planetList: &[Planet]) -> Vector3<f32> {
-        // Make sure planetList doesn't contain self
+    fn calc_accel(&self, planet_list: &[Planet]) -> Vector3<f32> {
+        // Make sure planet_list doesn't contain self
         let mut accel: Vector3<f32> = 0;
-        for p in planetList {
+        for p in planet_list {
             let dr = p.pos - self.pos;
             accel += p.mass * dr / dr.magnitude().powi(3);
         }
@@ -22,13 +29,23 @@ impl Planet {
     }
 
     // Do the actual moving
-    fn step(&mut self, planetList: &[Planet], dt: f32,) {
+    fn step(&mut self, planet_list: &[Planet], dt: f32,) {
         let pos_old = self.pos;
         let vel_old = self.vel;
-        let accel_old = calcAccel(planetList);
+
+        let accel_old = calc_accel(planet_list);
         self.pos = self.pos + self.vel * dt;                    // Expected pos
-        let accel_exp = calcAccel(planetList);
+        let accel_exp = calc_accel(planet_list);
         self.vel += (accel_old + accel_exp) * 0.5 * dt;         // New vel
         self.pos = pos_old + (self.vel + vel_old) * 0.5 * dt;   // New pos
+    }
+
+    // Attempt to calculate inital centripetal velocity for stable orbit
+    fn calc_init_velocity(&self, planet_list: &[Planet]) -> Vector3<f32> {
+        let accel = calc_accel(planet_list);
+        let star: Option<Planet> = None;
+        let dr = (star.unwrap().pos - self.pos).magnitude();
+        // INCMPLETE
+        f32::sqrt(accel.magnitude() * dr)
     }
 }
